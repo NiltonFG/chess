e8e7520e1735ba2450ee1439afb5bc6a6b11439d
niltonFG <nilton.felipe.gavazzi@gmail.com>
2020-09-09 16:35:26 -0300

    First class

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,8 +1,11 @@
 package application;
 
+import boardgame.Position;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Position position = new Position(1,5);
+        System.out.println(position);
     }
 }
